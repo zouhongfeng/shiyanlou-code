321afd8563ab3a31759983c372b0dcfd49dc1f46
zouhongfeng <18219112616@163.com>
2019-09-16 14:13:09 +0800

    first commit

---
+++ testhundred.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+number = int(input("Enter an integer:"))
+if number <= 100:
+    print('Your number is less than of equal to 100')
+else:
+    print('Your number is greather than 100')
